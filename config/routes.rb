cd01b674248349c30c38a1bfdc9806b01623e9c4
sam fallowfield <sam.fallowfield1@gmail.com>
2023-04-21 12:11:14 +0100

    created users signup page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'pages#home'
   resources :records
+  get 'signup', to:'users#new'
+  post 'users', to:'users#create'
+  resources :users, except:[:new]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_144151) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_21_081205) do
   create_table "records", force: :cascade do |t|
     t.string "artist"
     t.string "album"
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_144151) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
