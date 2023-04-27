72879c322f29c856176375030fa6b9838c5a99cc
sam fallowfield <sam.fallowfield1@gmail.com>
2023-04-27 09:17:41 +0100

    begin user validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:show, :update, :edit, :destroy]
 
   def new
     @user = User.new
   end
-
 
   def create
     @user = User.new(user_params)
@@ -15,8 +15,42 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @records = @user.records
+  end
+
+  # def show
+  #   @records = @user.records.paginate(page: params[:page], per_page: 8)
+  # end
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user = User.update(user_params)
+      flash[:notice] = "Account updated!"
+      redirect_to root_path
+    else
+      render edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
